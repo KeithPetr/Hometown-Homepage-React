f5effc61b39693c68f268b48713d81d5aed98554
KeithPetr <keith.petryshyn@gmail.com>
2023-04-27 15:18:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hometown-Homepage-React
+ 
---
+++ src/Top.jsx
@@ -0,0 +1,10 @@
+export default function Top() {
+  return (
+    <div className="hero">
+      <h1>Visit Lindsay</h1>
+      <h2>
+        Enjoy the outdoors and historic downtown center in this small town.
+      </h2>
+    </div>
+  );
+}
